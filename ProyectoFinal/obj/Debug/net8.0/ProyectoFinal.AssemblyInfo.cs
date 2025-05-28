d31425d990438e0c6f2cc0036c503976fcdcdabd
EDUARDOAVILAF <140217389+EDUARDOAVILAF@users.noreply.github.com>
2025-05-27 22:12:05 -0500

    finalizacion

--- ProyectoFinal/obj/Debug/net8.0/ProyectoFinal.AssemblyInfo.cs
+++ ProyectoFinal/obj/Debug/net8.0/ProyectoFinal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b151b80d7c8e09e9a8ecf904e1602c642d7cf267")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+503804cb98f452601d71e8bd4e8c9dec54d70333")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
